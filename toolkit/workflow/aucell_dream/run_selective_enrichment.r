e7d1e9b4042db9ac4aa317689afc600d70ca876e
Jonathan Talbot-Martin <jtalbotmartin@outlook.com>
2024-05-31 15:08:50 +0100

    split celltype by TREM2 variant to perform stratified selective enrichment analysis

--- toolkit/workflow/aucell_dream/run_selective_enrichment.r
+++ toolkit/workflow/aucell_dream/run_selective_enrichment.r
@@ -86,131 +86,149 @@ sce <- sce[!is.na(SummarizedExperiment::rowData(sce)$gene), ]
 sce <- sce[!duplicated(SummarizedExperiment::rowData(sce)$gene), ]
 rownames(sce) <- SummarizedExperiment::rowData(sce)$gene
 
-
-##  ............................................................................
-##  get celltype name and process expression matrix                         ####
-
 celltype <- gsub("_sce", "", tools::file_path_sans_ext(basename(args$sce)))
 
-cli::cli_text("Normalising expression matrix")
- 
-expr_mat <- scater::calculateCPM(sce, exprs_values = "counts")
-expr_mat <- as.matrix(log2(expr_mat + 1))
- 
 ##  ............................................................................
-##  get celltype name and process expression matrix                         ####
+##  split celltype sce by TREM2Variant, and perform analysis                ####
 
-cli::cli_text("Building cells rankings")
-# 
-cells_rankings <- AUCell_buildRankings(exprMat = expr_mat,
-                                       nCores= future::availableCores(),
-                                       plotStats=FALSE)
-# 
-dir.create("aucell")
-qs::qsave(cells_rankings, file = sprintf("aucell/%s_cells_rankings.qs", celltype))
+for(variant in unique(sce[["TREM2Variant"]])){
 
-# alt workflow: reading in pre-generated rankings
-  # cli::cli_text("Loading cells rankings")
-  # cells_rankings <- qs::qread(sprintf("aucell/%s_cells_rankings.qs", celltype))
+  cli::cli_h2("Subsetting {.strong {variant}} from TREM2Variant in {celltype}")
 
-cli::cli_text("Calculating AUC")
+  sce_subset <- sce[ , sce[["TREM2Variant"]] %in% variant]
+  sce_subset$manifest <- droplevels(sce_subset$manifest)
+  
+  sce_subset <- sce_subset[!is.na(SummarizedExperiment::rowData(sce_subset)$gene), ]
+  sce_subset <- sce_subset[!duplicated(SummarizedExperiment::rowData(sce_subset)$gene), ]
+  rownames(sce_subset) <- SummarizedExperiment::rowData(sce_subset)$gene
 
-set.seed(123)
-cells_AUC <- AUCell_calcAUC(geneSets = genesets_l, 
-                            rankings = cells_rankings,
-                            nCores = future::availableCores(), 
-                            aucMaxRank = ceiling(0.05 * nrow(cells_rankings)))
+  cli::cli_text("Normalising expression matrix")
+  
+  expr_mat <- scater::calculateCPM(sce_subset, exprs_values = "counts")
+  expr_mat <- as.matrix(log2(expr_mat + 1))
+  
+  ##  ............................................................................
+  ##  get celltype name and process expression matrix                         ####
 
-rm(cells_rankings)
-gc()
+  cli::cli_text("Building cells rankings for {variant}")
+  # 
+  cells_rankings <- AUCell_buildRankings(exprMat = expr_mat,
+                                        nCores= future::availableCores(),
+                                        plotStats=FALSE)
+  # 
 
-##  ............................................................................
-##  dream: process, run, write                                              ####
+  aucell_cellrankings_dir <- sprintf("%s/%s_%s/%s", outdir, "aucell_cell_rankings", geneset_name, celltype)
+  dir.create(aucell_cellrankings_dir, recursive = TRUE, showWarnings = FALSE)
+  qs::qsave(cells_rankings, file = sprintf("%s/%s_cells_rankings_%s.qs", aucell_cellrankings_dir, celltype, variant))
+  
+  # alt workflow: reading in pre-generated rankings
+    # cli::cli_text("Loading cells rankings")
+    # cells_rankings <- qs::qread(sprintf("aucell/%s_cells_rankings.qs", celltype))
 
-AUscore <- as.matrix(getAUC(cells_AUC))
+  cli::cli_text("Calculating AUC for {variant}")
 
-# dir.create("AUscore")
-# qs::qsave(AUscore, file = sprintf("AUscore/%s_auscore.qs", celltype))
+  set.seed(123)
 
-min_val <- 1
-AUscore_log <- log2(AUscore+min_val)
+  cells_AUC <- AUCell_calcAUC(geneSets = genesets_l, 
+                              rankings = cells_rankings,
+                              nCores = future::availableCores(), 
+                              aucMaxRank = ceiling(0.05 * nrow(cells_rankings)))
 
-rm(cells_AUC)
-gc()
+  rm(cells_rankings)
+  gc()
 
-metadata <- colData(sce) %>% as.data.frame()
-metadata <- metadata %>% 
-  mutate(total_features_by_counts = as.vector(scale(total_features_by_counts)),
-         pc_mito = as.vector(scale(pc_mito)),
-         Age = as.vector(scale(Age)),
-         PostMortemInterval = as.vector(scale(PostMortemInterval)),
-         NeuropathologicalDiagnosis = factor(NeuropathologicalDiagnosis, levels = c("Control", "AD")))
+  ##  ............................................................................
+  ##  dream: process, run, write                                              ####
 
-rm(sce)
-gc()
+  AUscore <- as.matrix(getAUC(cells_AUC))
 
-outdir_name <- sprintf("%s/%s_%s", outdir, "enrichment_res", geneset_name)
-dir.create(outdir_name)
+  aucell_auscore_dir <- sprintf("%s/%s_%s/%s", outdir, "aucell_au_scores", geneset_name, celltype)
+  dir.create(aucell_auscore_dir, recursive = TRUE)
+  qs::qsave(AUscore, file = sprintf("%s/%s_cells_rankings_%s.qs", aucell_auscore_dir, celltype, variant))
 
-param = SnowParam(future::availableCores(), "SOCK", progressbar=TRUE)
-register(param)
+  min_val <- 1
+  AUscore_log <- log2(AUscore+min_val)
 
-##  ............................................................................
-##  pct4G8PositiveArea                                                      ####
+  rm(cells_AUC)
+  gc()
 
-form <- ~ pct4G8PositiveArea + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
+  metadata <- colData(sce_subset) %>% as.data.frame()
+  metadata <- metadata %>% 
+    mutate(total_features_by_counts = as.vector(scale(total_features_by_counts)),
+          pc_mito = as.vector(scale(pc_mito)),
+          Age = as.vector(scale(Age)),
+          PostMortemInterval = as.vector(scale(PostMortemInterval)),
+          NeuropathologicalDiagnosis = factor(NeuropathologicalDiagnosis, levels = c("Control", "AD")))
 
-cli::cli_text(paste0("Calculating dream: ", form)
+  rm(sce_subset)
+  gc()
 
-fitmm = dream( AUscore_log, form, metadata)
+  outdir_name <- sprintf("%s/%s_%s/%s", outdir, "enrichment_res", geneset_name, celltype)
+  dir.create(outdir_name, recursive = TRUE, , showWarnings = FALSE)
 
-# note: coef is the column name / number specifying interest group: use column 2, because for categorical variables, an error occurs due to
-# all groups being added to the name of the variable
-res_dream <- topTable( fitmm, coef=2, number=Inf )
+  param = SnowParam(future::availableCores(), "SOCK", progressbar=TRUE)
+  register(param)
 
-res_dream$geneset <- rownames(res_dream)
-res_dream$form <- paste0(as.character(form), collapse = "")
-res_dream$celltype <- celltype
+  ##  ............................................................................
+  ##  pct4G8PositiveArea                                                      ####
 
-write.table(x = res_dream, 
-            file = sprintf("%s/%s_%s", outdir_name, celltype, "res_dream_pct4G8PositiveArea.tsv"),
-            sep = "\t", 
-            row.names = F) # row.names false to avoid issue with row and column name conflicts for excel users
+  form <- ~ pct4G8PositiveArea + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
 
-
-##  ............................................................................
-##  pctPHF1PositiveArea                                                     ####
-
-form <- ~ pctPHF1PositiveArea + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
-cli::cli_text(paste0("Calculating dream: ", form)
-
-fitmm = dream( AUscore_log, form, metadata)
-res_dream <- topTable( fitmm, coef=2, number=Inf )
-
-res_dream$geneset <- rownames(res_dream)
-res_dream$form <- paste0(as.character(form), collapse = "")
-res_dream$celltype <- celltype
-
-write.table(x = res_dream, 
-            file = sprintf("%s/%s_%s", outdir_name, celltype, "res_dream_pctPHF1PositiveArea.tsv"),
-            sep = "\t", 
-            row.names = F)
+  cli::cli_text("{variant}: Calculating dream {form}")
 
 
-##  ............................................................................
-##  NPD                                                                     ####
+  fitmm = dream( AUscore_log, form, metadata)
 
-form <- ~ NeuropathologicalDiagnosis + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
-cli::cli_text(paste0("Calculating dream: ", form)
+  # note: coef is the column name / number specifying interest group: use column 2, because for categorical variables, an error occurs due to
+  # all groups being added to the name of the variable
+  res_dream <- topTable( fitmm, coef=2, number=Inf )
 
-fitmm = dream( AUscore_log, form, metadata)
-res_dream <- topTable( fitmm, coef=2, number=Inf )
+  res_dream$geneset <- rownames(res_dream)
+  res_dream$form <- paste0(as.character(form), collapse = "")
+  res_dream$celltype <- celltype
 
-res_dream$geneset <- rownames(res_dream)
-res_dream$form <- paste0(as.character(form), collapse = "")
-res_dream$celltype <- celltype
+  write.table(x = res_dream, 
+              file = sprintf("%s/%s_%s_%s.tsv", outdir_name, celltype, "res_dream_pct4G8PositiveArea", variant),
+              sep = "\t", 
+              row.names = F) # row.names false to avoid issue with row and column name conflicts for excel users
 
-write.table(x = res_dream, 
-            file = sprintf("%s/%s_%s", outdir_name, celltype, "res_dream_NeuropathologicalDiagnosis.tsv"),
-            sep = "\t", 
-            row.names = F)
+
+  ##  ............................................................................
+  ##  pctPHF1PositiveArea                                                     ####
+
+  form <- ~ pctPHF1PositiveArea + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
+  cli::cli_text("{variant}: Calculating dream {form}")
+
+
+  fitmm = dream( AUscore_log, form, metadata)
+  res_dream <- topTable( fitmm, coef=2, number=Inf )
+
+  res_dream$geneset <- rownames(res_dream)
+  res_dream$form <- paste0(as.character(form), collapse = "")
+  res_dream$celltype <- celltype
+
+  write.table(x = res_dream, 
+              file = sprintf("%s/%s_%s_%s.tsv", outdir_name, celltype, "res_dream_pctPHF1PositiveArea", variant),
+              sep = "\t", 
+              row.names = F)
+
+
+  ##  ............................................................................
+  ##  NPD                                                                     ####
+
+  form <- ~ NeuropathologicalDiagnosis + (1| manifest) + total_features_by_counts + pc_mito + BrainRegion + Sex + Age + PostMortemInterval
+  cli::cli_text("{variant}: Calculating dream {form}")
+
+    
+  fitmm = dream( AUscore_log, form, metadata)
+  res_dream <- topTable( fitmm, coef=2, number=Inf )
+
+  res_dream$geneset <- rownames(res_dream)
+  res_dream$form <- paste0(as.character(form), collapse = "")
+  res_dream$celltype <- celltype
+
+  write.table(x = res_dream, 
+              file = sprintf("%s/%s_%s_%s.tsv", outdir_name, celltype, "res_dream_NeuropathologicalDiagnosis", variant),
+              sep = "\t", 
+              row.names = F)
+}
